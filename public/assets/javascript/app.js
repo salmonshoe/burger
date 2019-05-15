8508bef71314b2367021bc6648f5f8f903d9360f
salmonshoe <rafaelreynoso273@gmail.com>
2019-05-15 02:35:55 -0400

    twice

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,18 +1,34 @@
-$('.create-form').on('submit', (event) => {
-    event.preventDefault();
+$(function () {
+    $('.create-form').on('submit', (event) => {
+        event.preventDefault();
 
-    var orderUp = {
-        burger_name: $('#patty').val().trim(),
-        devoured: 0
-    };
+        let orderUp = {
+            burger_name: $('#patty').val().trim(),
+            devoured: 0
+        };
 
-    $.ajax('/api/burgers', {
-        type: 'POST',
-        data: orderUp
-    }).then(
-        function() {
-            console.log(`Rev up those fryers!`);
-            location.reload();
-        }
-    );
+        $.ajax('/api/burgers', {
+            type: 'POST',
+            data: orderUp
+        }).then(
+            function () {
+                console.log(`Rev up those fryers!`);
+                location.reload();
+            }
+        );
+    });
+
+    $('.delete').on('click', (event) => {
+        let id = $(this).data('id');
+
+        $.ajax(`/api/burgers/${id}` {
+            type: 'DELETE'
+        }).then(
+            function() {
+                console.log('delete id', id);
+                location.reload();
+            }
+        );
+    });
+    //update form
 });
